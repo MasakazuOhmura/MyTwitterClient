f558fef7204b6b7ff8248e3d776688723d01a59d
MasakazuOhmura <tourokusaito.masakazu@gmail.com>
2016-06-24 04:47:51 +0900

    add customview

--- app/src/main/java/com/masakazuohmura/mytwitterclient/MainActivity.java
+++ app/src/main/java/com/masakazuohmura/mytwitterclient/MainActivity.java
@@ -3,62 +3,20 @@ package com.masakazuohmura.mytwitterclient;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.*;
-import com.twitter.sdk.android.core.models.Search;
-import com.twitter.sdk.android.core.models.Tweet;
-import com.twitter.sdk.android.core.services.SearchService;
-import com.twitter.sdk.android.core.services.params.Geocode;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/**
+ * Created by MasakazuOhmura on 2016/06/21.
+ */
 public class MainActivity extends AppCompatActivity {
-
-    private TwitterTimelineAdapter mAdapter;
-    private ArrayList<Tweet> mTweets = new ArrayList<>();
-
-    // API GET search/tweets
-    private String q = "iQON";
-    private Geocode geocode = null;
-    private String lang = "ja";
-    private String local = "ja";
-    private String resultType = "recent";
-    private Integer count = 10;
-    private String until = null;
-    private Long sinceId = null;
-    private Long maxId = null;
-    private Boolean includeEntries = null;
-    private Callback<Search> cb = new Callback<Search>() {
-        @Override
-        public void success(Result<Search> result) {
-            for (int i = 0; i < result.data.tweets.size(); i++) {
-                Tweet tweet = result.data.tweets.get(i);
-                mTweets.add(tweet);
-
-                if (i == result.data.tweets.size() - 1) {
-                    maxId = tweet.id - 1L;
-                }
-            }
-            mAdapter.notifyDataSetChanged();
-        }
-
-        @Override
-        public void failure(TwitterException exception) {
-            Log.e("Twitter Exception", exception.getMessage());
-        }
-    };
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
     @BindView(R.id.recyclerView)
-    RecyclerView mRecyclerView;
+    TwitterTimelineRecyclerView mTwitterTimelineRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,37 +26,13 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(mToolbar);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mAdapter = new TwitterTimelineAdapter(mTweets, MainActivity.this);
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, R.drawable.recyclerview_divider));
-
-        final TwitterAuthToken authToken = new TwitterAuthToken(BuildConfig.TWITTER_TOKEN, BuildConfig.TWITTER_SECRET_TOKEN);
-        final TwitterSession session = new TwitterSession(authToken, BuildConfig.TWITTER_USER_ID, BuildConfig.TWITTER_USER_NAME);
-        final TwitterApiClient twitterApiClient = Twitter.getApiClient(session);
-        final SearchService searchService = twitterApiClient.getSearchService();
-        requestSearchApi(searchService);
-
-        mRecyclerView.addOnScrollListener(new EndlessScrollListener((LinearLayoutManager) mRecyclerView.getLayoutManager()) {
+        mTwitterTimelineRecyclerView.getTweets();
+        mTwitterTimelineRecyclerView.addOnScrollListener(new EndlessScrollListener((LinearLayoutManager) mTwitterTimelineRecyclerView.getLayoutManager()) {
             @Override
             public void onLoadMore(int current_page) {
-                requestSearchApi(searchService);
+                mTwitterTimelineRecyclerView.getTweets();
             }
         });
     }
 
-    private void requestSearchApi(SearchService searchService) {
-        searchService.tweets(q,
-                geocode,
-                lang,
-                local,
-                resultType,
-                count,
-                until,
-                sinceId,
-                maxId,
-                includeEntries,
-                cb);
-    }
 }
--- app/src/main/java/com/masakazuohmura/mytwitterclient/TwitterTimelineRecyclerView.java
+++ app/src/main/java/com/masakazuohmura/mytwitterclient/TwitterTimelineRecyclerView.java
@@ -1,7 +1,115 @@
 package com.masakazuohmura.mytwitterclient;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.models.Search;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.services.SearchService;
+import com.twitter.sdk.android.core.services.params.Geocode;
+
+import java.util.ArrayList;
+
 /**
  * Created by MasakazuOhmura on 2016/06/24.
  */
-public class TwitterTimelineRecyclerView {
+public class TwitterTimelineRecyclerView extends RecyclerView {
+
+    private SearchService mSearchService;
+
+    private TwitterTimelineAdapter mAdapter;
+    private ArrayList<Tweet> mTweets = new ArrayList<>();
+
+    // API GET search/tweets
+    private String q = "iQON";
+    private Geocode geocode = null;
+    private String lang = "ja";
+    private String local = "ja";
+    private String resultType = "recent";
+    private Integer count = 10;
+    private String until = null;
+    private Long sinceId = null;
+    private Long maxId = null;
+    private Boolean includeEntries = null;
+    private Callback<Search> cb = new Callback<Search>() {
+        @Override
+        public void success(Result<Search> result) {
+            for (int i = 0; i < result.data.tweets.size(); i++) {
+                Tweet tweet = result.data.tweets.get(i);
+                mTweets.add(tweet);
+
+                if (i == result.data.tweets.size() - 1) {
+                    maxId = tweet.id - 1L;
+                }
+            }
+            mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void failure(TwitterException exception) {
+            Log.e("Twitter Exception", exception.getMessage());
+        }
+    };
+
+    public TwitterTimelineRecyclerView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public TwitterTimelineRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public TwitterTimelineRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        twitterInit();
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+        this.setLayoutManager(layoutManager);
+        this.addItemDecoration(new DividerItemDecoration(context, R.drawable.recyclerview_divider));
+
+        mAdapter = new TwitterTimelineAdapter(mTweets, context);
+        this.setAdapter(mAdapter);
+    }
+
+    private void twitterInit() {
+        final TwitterAuthToken authToken = new TwitterAuthToken(BuildConfig.TWITTER_TOKEN, BuildConfig.TWITTER_SECRET_TOKEN);
+        final TwitterSession session = new TwitterSession(authToken, BuildConfig.TWITTER_USER_ID, BuildConfig.TWITTER_USER_NAME);
+        final TwitterApiClient twitterApiClient = Twitter.getApiClient(session);
+        mSearchService = twitterApiClient.getSearchService();
+    }
+
+    public void getTweets() {
+        requestSearchApi(mSearchService);
+    }
+
+    private void requestSearchApi(SearchService searchService) {
+        searchService.tweets(q,
+                geocode,
+                lang,
+                local,
+                resultType,
+                count,
+                until,
+                sinceId,
+                maxId,
+                includeEntries,
+                cb);
+    }
 }
